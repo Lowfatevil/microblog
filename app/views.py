17675635afded067d1d8cab4d1572130d7192d8b
Lowfatevil <terence.callaghan@gmail.com>
2015-12-05 18:57:32 +0000

    update with template handling

--- app/views.py
+++ app/views.py
@@ -1,16 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'} # fake user
-    return '''
-<html>
-    <head>
-        <title>Home Page</title>
-    </head>
-    <body>
-     <h1> Hello, ''' + user['nickname'] + '''</h1>
-    </body>
-</html>
-'''
+    posts = [ #fake array of fake post from fake user
+            {
+                'author':{'nickname':'John'},
+                'body':'Beautiful day in Portland!'
+            },
+            {
+                'author':{'nickname':'Susan'},
+                'body' : 'The Avengers movie was so cool!'
+                }
+            ]
+    return render_template('index.html',
+                            user=user,
+                            posts=posts)
+
