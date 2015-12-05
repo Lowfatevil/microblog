9d7dee911bb84489862b676ba038364fdd80c07d
Lowfatevil <terence.callaghan@gmail.com>
2015-12-05 21:48:23 +0000

    Updating depracated import calls wtforms

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
-from flask.ext.wtf import Form
-from wtfforms import StringField, BooleanField
-from wtffroms.validators import DataRequired
+from flask_wtf import Form
 
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
 
 class LoginForm(Form):
     openid = StringField('openid', validators = [DataRequired()])
--- app/views.py
+++ app/views.py
@@ -1,8 +1,19 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
+
+# index view function suppressed for brevity
 
 @app.route('/')
 @app.route('/index')
+@app.route('/login', methods=['GET','POST'])
+
+def login():
+    form = LoginForm()
+    return render_template('login.html',
+                            title='Sign In',
+                            form=form)
+
 def index():
     user = {'nickname': 'Miguel'} # fake user
     posts = [ #fake array of fake post from fake user
